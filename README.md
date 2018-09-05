b1834d5c98a89cb52a8e339c814eca7b198cc5b2
Lucas Moreira <lucas@moreiradevelopment.io>
2018-09-05 11:23:16 -0400

    Initial commit for new Pug + SASS pipeline

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# pug-sass-templating-boilerplate
-Pug + SASS Templating boilerplate for creating HTML5 and SCSS static front-end presentations.
+![MDEV Digital](https://mdev.digital/social/githubLogo.svg)
+
+# Pug + SASS Templating Boilerplate
+## Webpack powered templating boilerplate for generating HTML pages.
+
+### Running Project Locally
+---
+
+### [ MDEV Digital ]
+We provide process-driven UX/UI + development to help our clients unleash their potential to connect with their audience. Our not-so-secret sauce is gaining a fundamental understanding of the challenges and opportunities our clients present us to provide innovative custom solutions using the latest web application technologies.
+
+### [ Contact ]
+[MDEV DIgital Website](http://mdev.digital).
+
+[Contact Support](mailto:contact@mdev.digital).
+
+Phone: +1(519)-860-4261
+
+
---
+++ webpack.common.js
@@ -0,0 +1,130 @@
+// MDEV Digital - Webpack Boilerplate[VueJS]
+// Webpack 4 Configuration file
+// -----------------------------------------
+// PRODUCTION ENVIRONMENT
+// ----------------------------------------
+
+// Required Imports
+const path = require('path')
+const webpack = require('webpack')
+const StyleLintPlugin = require('stylelint-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const HtmlWebpackPlugin = require ('html-webpack-plugin')
+
+const setPath = function(folderName) {
+  return path.join(__dirname, folderName);
+}
+
+// Module Exports
+module.exports = {
+  // Asset Splitting [ Vendor | Build ]
+  entry: {
+    build: './index.js'
+  },
+  // Output Files
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/',
+    filename: 'app/[name][hash].js'
+  },
+  // Split Vendor | Build Assets into separate chunks
+  optimization:{
+    runtimeChunk: false,
+    splitChunks: {
+      chunks: "all", //Taken from https://gist.github.com/sokra/1522d586b8e5c0f5072d7565c2bee693
+    }
+  },
+  // Find Node Modules
+  resolveLoader: {
+    modules: [setPath('/node_modules')]
+  },
+  // Module Rules & Loaders
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+          exclude: /(node_modules)/,
+          use: [{
+            loader: "babel-loader",
+            options: { presets: ['es2015'] }
+          }]
+      },
+      // CSS Processing
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+        fallback: "style-loader",
+          use: ["css-loader"]
+        })
+      },
+      // SASS Processing
+      {
+        test: /\.scss$/,
+        use:
+        ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 2 // 0 => no loaders (default); 1 => postcss-loader; 2 => postcss-loader, sass-loader
+              }
+            },
+            'postcss-loader',
+            'sass-loader'
+          ]
+        }),
+      },
+      // Image Processing
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        loaders: [ 'file-loader?context=src/images&name=images/[path][name].[ext]', {
+          loader: 'image-webpack-loader',
+          query: {
+            // JPEG Processing
+            mozjpeg: {
+              progressive: true,
+              quality: 90
+            },
+            // GIF Processing
+            gifsicle: {
+              interlaced: false,
+              optimizationLevel: 2
+            },
+            // PNG Processing
+            pngquant: {
+              quality: '75-80',
+              speed: 2
+            },
+            // SVG Processing
+            svgo: {
+              plugins: [
+                {
+                  removeViewBox: false
+                },
+                {
+                  removeEmptyAttrs: false
+                }
+              ]
+            }
+          }
+        }]
+      }
+    ]
+  },
+  // Plugins & Post Processing
+  plugins: [
+    new StyleLintPlugin({
+      syntax: 'scss',
+      files: ['**/*.vue']
+    }),
+    // Text Extraction & Chunking
+    new ExtractTextPlugin("assets/styles/styles[hash].css"),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+  ],
+  performance: {
+    hints: 'warning'
+  }
+}
