1f79607381bb767c1c760998f8ce21c5194434f8
Lucas Moreira <lucas@moreiradevelopment.io>
2019-02-04 11:24:25 -0500

    Updated Layouts & Analytics - Added Google Analytics - Added Google Tag Manager - Added default Layout

---
+++ src/js/GA-setup.js
@@ -0,0 +1,6 @@
+// G Analytics
+// TODO - EDIT GA_TRACKING_ID
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'GA_TRACKING_ID');
